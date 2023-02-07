ddf055f91418068f64783a713ae26c3a994e4d63
hyun <hgh6128@gmail.com>
2023-02-07 16:42:44 +0900

    라우터 초기 구성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Router>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
