eb352c66ec71dd7f1222a547ebc229ee041d3aa3
hyun <hgh6128@gmail.com>
2023-02-13 15:47:05 +0900

    버튼수정

--- src/components/Detail/ButtonGroup.js
+++ src/components/Detail/ButtonGroup.js
@@ -6,27 +6,15 @@ const ButtonGroup = ({ filters, arr, setFilter }) => {
       {arr.map((item, index) => (
         <button
           type="button"
-          className={`
-       
-    
-     px-6
-     
-     py-2
-     my-3
-      border-blue-600
-     text-main
-     font-medium
-     text-xs
-     leading-tight
-    
-   
-    ${filters === item.title && "bg-blue-200"}`}
+          className={`px-6 py-2 my-3 text-main text-xs leading-tight ${
+            filters === item.title && "bg-blue-200"
+          }`}
           key={index}
           onClick={() => {
             setFilter(item.title);
           }}
         >
-          <div className="relative flex ">
+          <div className="relative flex items-center gap-1">
             <p className="text-xs">{item.title}</p>
             <div>{item.icon}</div>
           </div>
