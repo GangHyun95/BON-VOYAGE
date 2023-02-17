a694694a09ba106f3db8c5ec564cc9f1fcc84826
zuyoe <9xtwo5@naver.com>
2023-02-17 12:51:44 +0900

    회원가입이랑기타등등

--- src/components/MyPage/EditProfile.js
+++ src/components/MyPage/EditProfile.js
@@ -34,14 +34,14 @@ const EditProfile = ({ closeModal }) => {
   };
 
   return (
-    <div className="p-16 flex items-center flex-col ">
-      <h1 className="relative w-full text-xl mb-6 text-center">
+    <div className="w-[900px] h-[700px] p-20  flex items-center flex-col ">
+      <div className="relative w-full text-xl mb-6 ">
         회원정보수정
         <GrClose
           className="absolute top-1/2 -translate-y-1/2 right-0"
           onClick={closeModal}
         />
-      </h1>
+      </div>
 
       <form className="flex flex-col">
         <label className="block text-xs">이메일</label>
@@ -64,25 +64,19 @@ const EditProfile = ({ closeModal }) => {
           value={password}
           onChange={handlePasswordChange}
           className="bg-gray-100 text-xs px-[20px] w-[450px] py-3 my-3 rounded-lg"
-          placeholder="비밀번호(문자, 숫자, 특수문자 포함 10~20자)"
         />
-        <label className="block text-xs">비밀번호 확인 </label>
-        <input
-          className="bg-gray-100 text-xs text-left px-[20px] w-[450px] py-3 my-3 rounded-lg"
-          type="password"
-          value={passwordVerify}
-          onChange={handlePasswordVerifyChange}
-          placeholder="비밀번호 재입력"
-        />
-        <button className="w-full bg-main text-white rounded-lg py-2 my-3">
+        
+        <div className="flex justify-center gap-5">
+        <button className="w-[100px] h-[50px] bg-main text-white rounded-lg py-2 ">
           수정
         </button>
         <button
-          className="bg-gray-500 text-white py-2 rounded-lg"
+          className="w-[100px] h-[50px] bg-gray-500 text-white py-2 rounded-lg"
           type="button"
         >
           취소
         </button>
+        </div>
       </form>
     </div>
   );
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,140 +2,154 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router";
 
 const SignUp = () => {
-  const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordVerify, setPasswordVerify] = useState("");
+    const navigate = useNavigate();
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordVerify, setPasswordVerify] = useState("");
+    const [name, setName] = useState("");
+    const [nickname, setNickname] = useState("");
+    const [number, setNumber] = useState("");
+    const [email, setEmail] = useState("");
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
+    const handleIdChange = (event) => {
+        setId(event.target.value);
+    }; 
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+    const handlePasswordVerifyChange = (event) => {
+        setPasswordVerify(event.target.value);
+    };
+    const handleNameChange = (event) => {
+      setName(event.target.value);
+  };
+  const handleNicknameChange = (event) => {
+    setNickname(event.target.value);
+};
+const handleNumberChange = (event) => {
+  setNumber(event.target.value);
+};
+
+
+
+    const handleEmailChange = (event) => {
+      setEmail(event.target.value);
   };
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (password !== passwordVerify) {
+            alert("비밀번호 확인이 일치하지않습니다.");
+            return;
+        }
+        alert(`이메일: ${email}\비밀번호: ${password} 회원가입 되었습니다. 로그인해주세요.`);
+    };
+    const [isChecked, setIsChecked] = useState(false);
+    const [isChecked2, setIsChecked2] = useState(false);
+    const [isChecked3, setIsChecked3] = useState(false);
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handlePasswordVerifyChange = (event) => {
-    setPasswordVerify(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (password !== passwordVerify) {
-      alert("비밀번호 확인이 일치하지않습니다.");
-      return;
-    }
-    alert(
-      `이메일: ${email}\비밀번호: ${password} 회원가입 되었습니다. 로그인해주세요.`
+    const handleCheckboxChange = (event) => {
+        setIsChecked(event.target.checked);
+    };
+    const handleCheckboxChange2 = (event) => {
+        setIsChecked2(event.target.checked);
+    };
+    const handleCheckboxChange3 = (event) => {
+        setIsChecked3(event.target.checked);
+    };
+    return (
+        <div className="relative m-[200px] flex items-center flex-col ">
+            <h1 className=" my-[50px] text-xl">Sign Up</h1>
+            <form onSubmit={handleSubmit}>
+                <label className="block text-xs font-bold text-slate-600">아이디</label>
+                <input
+                    type="text"
+                    value={id}
+                    onChange={handleIdChange}
+                    placeholder="아이디 입력(6-18글자)"
+                    maxLength={18}
+                    className="bg-gray-100 px-[20px] text-xs w-[450px] py-3 my-3 rounded-lg relative"
+                />
+                <button className="absolute  right-20 top-40 rounded-md 
+                bg-[#4C7EFF] text-white p-2 text-xs">중복확인</button>
+                <br />
+                <label className="block text-xs font-bold text-slate-600">비밀번호 </label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    className="bg-gray-100 text-xs px-[20px] w-[450px] py-3 my-3 rounded-lg"
+                    placeholder="비밀번호(문자, 숫자, 특수문자 포함 5~16자)"
+                    maxLength={16}
+                />
+                <br />
+                <label className="block text-xs font-bold text-slate-600">비밀번호 확인 </label>
+                <input
+                    className="bg-gray-100 text-xs text-left px-[20px] w-[450px] py-3 my-3 rounded-lg"
+                    type="password"
+                    value={passwordVerify}
+                    onChange={handlePasswordVerifyChange}
+                    placeholder="비밀번호 재입력"
+                    maxLength={16}
+                />
+                <br />
+                <label className="block text-xs font-bold text-slate-600">이름</label>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={handleNameChange}
+                    placeholder="이름 입력"
+                    className="bg-gray-100 px-[20px] text-xs w-[450px] py-3 my-3 rounded-lg"
+                />
+                <br />
+                <label className="block text-xs font-bold text-slate-600">닉네임</label>
+                <input
+                    type="text"
+                    value={nickname}
+                    onChange={handleNicknameChange}
+                    placeholder="닉네임 입력"
+                    className="bg-gray-100 px-[20px] text-xs w-[450px] py-3 my-3 rounded-lg"
+                    maxLength={16}
+                />
+                <br />
+                <label className="block text-xs font-bold text-slate-600">전화번호</label>
+                <input
+                    type="text"
+                    value={number}
+                    onChange={handleNumberChange}
+                    placeholder="휴대폰 번호 입력(‘-’ 제외 11자리 입력)"
+                    className="bg-gray-100 px-[20px] text-xs w-[450px] py-3 my-3 rounded-lg"
+                    maxLength={11}
+                />
+                <br />
+                <label className="block text-xs font-bold text-slate-600">이메일 </label>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    placeholder="영문,숫자,특수문자"
+                    className="bg-gray-100 px-[20px] text-xs w-[450px] py-3 my-3 rounded-lg"
+                    maxLength={30}
+                />
+                <br />
+                <div className="flex justify-center space-x-5 font-bold">
+                    <button
+                        type="submit"
+                        className="bg-main block rounded-lg w-[150px] my-4 text-white py-3"
+                    >
+                        회원가입
+                    </button>
+                    <button
+                        type="button"
+                        className="bg-gray-500 rounded-lg w-[150px] my-4 text-white py-3"
+                        onClick={(e) => navigate(-1)}
+                    >
+                        뒤로가기
+                    </button>
+                </div>
+            </form>
+        </div>
     );
-  };
-  const [isChecked, setIsChecked] = useState(false);
-  const [isChecked2, setIsChecked2] = useState(false);
-  const [isChecked3, setIsChecked3] = useState(false);
-
-  const handleCheckboxChange = (event) => {
-    setIsChecked(event.target.checked);
-  };
-  const handleCheckboxChange2 = (event) => {
-    setIsChecked2(event.target.checked);
-  };
-  const handleCheckboxChange3 = (event) => {
-    setIsChecked3(event.target.checked);
-  };
-  return (
-    <div className="relative m-[200px] flex items-center flex-col ">
-      <h1 className=" my-[50px] text-xl">Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <label className="block text-xs">이메일 </label>
-        <input
-          type="email"
-          value={email}
-          onChange={handleEmailChange}
-          className="bg-gray-100 px-[20px] text-xs w-[450px] py-3 my-3 rounded-lg"
-        />
-
-        <br />
-        <label className="block text-xs">이름</label>
-        <input
-          type="text"
-          value={name}
-          onChange={handleNameChange}
-          className="bg-gray-100 px-[20px] text-xs w-[450px] py-3 my-3 rounded-lg"
-        />
-
-        <br />
-        <label className="block text-xs">비밀번호 </label>
-        <input
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-          className="bg-gray-100 text-xs px-[20px] w-[450px] py-3 my-3 rounded-lg"
-          placeholder="비밀번호(문자, 숫자, 특수문자 포함 10~20자)"
-        />
-
-        <br />
-        <label className="block text-xs">비밀번호 확인 </label>
-        <input
-          className="bg-gray-100 text-xs text-left px-[20px] w-[450px] py-3 my-3 rounded-lg"
-          type="password"
-          value={passwordVerify}
-          onChange={handlePasswordVerifyChange}
-          placeholder="비밀번호 재입력"
-        />
-
-        <br />
-        <div>
-          <label className="text-xs text-gray-600">
-            <input
-              type="checkbox"
-              checked={isChecked}
-              onChange={handleCheckboxChange}
-            />
-            본인은 만 14세 이상입니다.<span className="text-main"> (필수)</span>
-          </label>
-        </div>
-        <div>
-          <label className="text-xs text-gray-600">
-            <input
-              type="checkbox"
-              checked={isChecked2}
-              onChange={handleCheckboxChange2}
-            />
-            개인정보수집에 동의합니다.{" "}
-            <span className="text-main"> (필수)</span>
-          </label>
-        </div>
-        <div>
-          <label className="text-xs text-gray-600">
-            <input
-              type="checkbox"
-              checked={isChecked3}
-              onChange={handleCheckboxChange3}
-            />
-            이용약관에 동의합니다. <span className="text-main"> (필수)</span>
-          </label>
-        </div>
-        <button
-          type="submit"
-          className="bg-main block rounded-lg px-[195px] my-4 text-white py-3"
-        >
-          회원가입
-        </button>
-        <button
-          type="button"
-          className="bg-gray-500 rounded-lg px-[195px] my-4 text-white py-3"
-          onClick={(e) => navigate(-1)}
-        >
-          뒤로가기
-        </button>
-      </form>
-    </div>
-  );
 };
 
 export default SignUp;
